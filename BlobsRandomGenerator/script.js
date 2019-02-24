5409d5e1763459a750b54c3e0e466d7a613a9690
Dejan <ivkovicd69@gmail.com>
2019-02-24 20:35:17 +0100

    Deleted some lines

--- BlobsRandomGenerator/script.js
+++ BlobsRandomGenerator/script.js
@@ -3,18 +3,16 @@ function generateBlob() {
   var percentage2 = _.random(25, 75);
   var percentage3 = _.random(25, 75);
   var percentage4 = _.random(25, 75);
-  var percentage5 = _.random(25, 70);
-  var percentage6 = _.random(25, 70);
+ 
 
   var percentage11 = 100 - percentage1;
   var percentage21 = 100 - percentage2;
   var percentage31 = 100 - percentage3;
   var percentage41 = 100 - percentage4;
-  var percentage51 = 100 - percentage5;
-  var percentage61 = 100 - percentage6;
+  
 
 
-  var borderRadius = ` ${percentage1}% ${percentage11}% ${percentage21}% ${percentage2}%  / ${percentage5}% ${percentage61}% ${percentage6}% ${percentage51}%`;
+  var borderRadius = ` ${percentage1}% ${percentage11}% ${percentage21}% ${percentage2}%  / ${percentage3}% ${percentage41}% ${percentage4}% ${percentage31}%`;
 
   $(".blob").css("border-radius", borderRadius);
   $(".style span").html(borderRadius);
